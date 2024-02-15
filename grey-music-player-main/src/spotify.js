30c8d2b739929576e4fb56c68b773c0b0cb917ab
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2024-02-15 09:24:01 +0530

    Initial Commit

---
+++ grey-music-player-main/src/spotify.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const clientId = "5f03f0ddc33a4e56b25a560d82570b93";
+const redirectUri = "http://localhost:3000";
+const scopes = ["user-library-read", "playlist-read-private"];
+
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
+
+const apiClient = axios.create({
+  baseURL: "https://api.spotify.com/v1/",
+});
+
+export const setClientToken = (token) => {
+  apiClient.interceptors.request.use(async function (config) {
+    config.headers.Authorization = "Bearer " + token;
+    return config;
+  });
+};
+
+export default apiClient;
